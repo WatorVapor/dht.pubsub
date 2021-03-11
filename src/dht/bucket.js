ff33c6b6ca1ca7445fbe910527ba3715dec7888e
Wator Vapor <watovapor@gmail.com>
2021-03-11 13:00:10 +0000

    pure dht

---
+++ src/dht/bucket.js
@@ -0,0 +1,167 @@
+'use strict';
+const base32 = require("base32.js");
+const bitwise = require("bitwise");
+const bigInt = require("big-integer");
+const iConstBucketMax = 4;
+const DHTUtils = require('./utils.js');
+const utils = new DHTUtils();
+const MaxBitOf160 = [];
+for(let i = 0;i <160;i++) {
+  MaxBitOf160.push(1);
+}
+//console.log(':: MaxBitBufOf160.length=<',MaxBitOf160.length,'>');
+const MaxBitBufOf160 = bitwise.buffer.create(MaxBitOf160);
+//console.log(':: MaxBitBufOf160=<',MaxBitBufOf160,'>');
+const MaxBigIntOf160 = bigInt(MaxBitBufOf160.toString('hex'),16);
+//console.log(':: MaxBigIntOf160=<',MaxBigIntOf160,'>');
+
+class DHTBucket {
+  constructor(node) {
+    //console.log('DHTBucket::constructor node=<',node,'>');
+    this.node_ = node;
+    this.id_ = this.node_.id;
+    const idBuf = base32.decode(this.id_);
+    const idBit = bitwise.buffer.read(idBuf);
+    //console.log('DHTBucket::constructor idBit=<',idBit,'>');
+    this.buckets_ = [];
+    for(const bit of idBit) {
+      this.buckets_.push([]);
+    }
+    //console.log('DHTBucket::constructor this.buckets_=<',this.buckets_,'>');
+    this.buckets_flat_ = [];
+    this.buckets_flat_.push(this.id_);
+  }
+  update(node,endpoint) {
+    //console.log('DHTBucket::update node=<',node,'>');
+    //console.log('DHTBucket::update endpoint=<',endpoint,'>');
+    if(endpoint.ttl > 1000 && endpoint.trap) {
+      return;
+    }
+    const escape_ms = new Date() - new Date(endpoint.at);
+    //console.log('DHTBucket::update escape_ms=<',escape_ms,'>');
+    if(escape_ms > 1000) {
+      return;
+    }
+    //console.log('DHTBucket::update node=<',node,'>');
+    const nodeBuf = base32.decode(node);
+    //console.log('DHTBucket::update nodeBuf=<',nodeBuf,'>');
+    const nodeBit = bitwise.buffer.read(nodeBuf);
+    //console.log('DHTBucket::update nodeBit=<',nodeBit,'>');
+    const firstAt = nodeBit.indexOf(1);
+    //console.log('DHTBucket::update firstAt=<',firstAt,'>');
+    if(firstAt > -1) {
+      const bucketCap = Math.floor((nodeBit.length - firstAt)/16) + 2;
+      //console.log('DHTBucket::update bucketCap=<',bucketCap,'>');
+      if(!this.buckets_[firstAt].includes(node)  
+        && this.buckets_[firstAt].length < bucketCap
+      ) {
+        this.buckets_[firstAt].push(node);
+        this.buckets_flat_.push(node);
+      }
+    }
+    //console.log('DHTBucket::update this.buckets_flat_=<',this.buckets_flat_,'>');
+  }
+  remove(node) {
+    //console.log('DHTBucket::remove node=<',node,'>');
+    const flatHint = this.buckets_flat_.indexOf(node);
+    if(flatHint > -1) {
+      //console.log('DHTBucket::remove node=<',node,'>');
+      //console.log('DHTBucket::remove flatHint=<',flatHint,'>');
+      this.buckets_flat_.splice(flatHint,1);
+    }
+    for(const bucketIndex in this.buckets_) {
+      //console.log('DHTBucket::remove bucketIndex=<',bucketIndex,'>');
+      const hint = this.buckets_[bucketIndex].indexOf(node);
+      if(hint > -1) {
+        //console.log('DHTBucket::remove node=<',node,'>');
+        //console.log('DHTBucket::remove hint=<',hint,'>');
+        this.buckets_[bucketIndex].splice(hint,1);
+      }
+    }
+  }
+  near(address) {
+    console.log('DHTBucket::near address=<',address,'>');
+    const address2 = utils.calcAddress(address);
+    const address3 = utils.calcAddress(address2);
+    return this.near_(address,address2,address3);
+  }
+  near_(add1,add2,add3) {
+    let near1 = false;
+    let near2 = false;
+    let near3 = false;
+    let value1 = MaxBigIntOf160;
+    let value2 = MaxBigIntOf160;
+    let value3 = MaxBigIntOf160;
+    //console.log('DHTBucket::near_ value1=<',value1,'>');
+    for(const nodeId of this.buckets_flat_) {
+      //console.log('DHTBucket::near_ nodeId=<',nodeId,'>');
+      //const endPoint = this.buckets_flat_[nodeId];
+      const distance1 = this.calcDistance_(add1,nodeId);
+      //console.log('DHTBucket::near_ distance1=<',distance1,'>');
+      if(distance1 < value1 ) {
+        value1 = distance1;
+        near1 = nodeId;
+      }
+      const distance2 = this.calcDistance_(add2,nodeId);
+      //console.log('DHTBucket::near_ distance2=<',distance2,'>');
+      if(distance2 < value2 ) {
+        value2 = distance2;
+        near2 = nodeId;
+      }
+      const distance3 = this.calcDistance_(add3,nodeId);
+      //console.log('DHTBucket::near_ distance3=<',distance3,'>');
+      if(distance3 < value3 ) {
+        value3 = distance3;
+        near3 = nodeId;
+      }
+    }
+    const nearGate = [];
+    nearGate.push(near1);
+    nearGate.push(near2);
+    nearGate.push(near3);
+    return nearGate;
+  }
+
+
+  calcDistance_(address,peer) {
+    //console.log('PeerRoute::calcDistance_ address=<',address,'>');
+    //console.log('PeerRoute::calcDistance_ peer=<',peer,'>');
+    const addressBuf = base32.decode(address);
+    const peerBuf = base32.decode(peer);
+    const distanceBuf = bitwise.buffer.xor(addressBuf,peerBuf,false);
+    //console.log('PeerRoute::calcDistance_ distanceBuf=<',distanceBuf,'>');
+    return bigInt(distanceBuf.toString('hex'),16);
+    
+    /*
+    const distanceBit = bitwise.buffer.read(distanceBuf);
+    //console.log('PeerRoute::calcDistance_ distanceBit=<',distanceBit,'>');
+    
+    let distanceXor = 0;
+    for(const bit of distanceBit) {
+      if(bit) {
+        distanceXor++;
+      }
+    }
+    //console.log('PeerRoute::calcDistance_ distanceXor=<',distanceXor,'>');
+    return distanceXor;
+    */
+  }
+  calcDistanceBit_(address,peer) {
+    const addressBuf = base32.decode(address);
+    const peerBuf = base32.decode(peer);
+    const distanceBuf = bitwise.buffer.xor(addressBuf,peerBuf,false);
+    //console.log('PeerRoute::calcDistance_ distanceBuf=<',distanceBuf,'>');   
+    const distanceBit = bitwise.buffer.read(distanceBuf);
+    //console.log('PeerRoute::calcDistance_ distanceBit=<',distanceBit,'>'); 
+    let firstBit = -1;
+    for(const bit of distanceBit) {
+      firstBit++;
+      if(bit) {
+        break;
+      }
+    }
+    //console.log('PeerRoute::calcDistance_ firstBit=<',firstBit,'>');
+    return firstBit;
+  }  
+};
+module.exports = DHTBucket;
---
+++ src/dht/udp.js
@@ -0,0 +1,299 @@
+'use strict';
+const dgram = require('dgram');
+const DHTMachine = require('./machine.js');
+const debug_ = true;
+const NODE_LOST_TIME_OUT_MS = 5*1000;
+const NODE_LOST_TIME_OUT_MAX = 10;
+
+class DHTUdp {
+  constructor(conf,node,bucket,onMsg) {
+    if(debug_) {
+      console.log('DHTUdp::constructor: conf =<',conf,'>');
+    }
+    this.conf_ = conf;
+    this.client_ = dgram.createSocket('udp6');
+    this.onMsg_ = onMsg;
+    this.machine_ = new DHTMachine({localhost:false});
+    setTimeout(this.enterMesh_.bind(this),1000);
+    this.node_ = node;    
+    this.bucket_ = bucket;
+    this.worldNodes_ = {};
+    setInterval(this.doDHTPing_.bind(this),1*1000);
+  }
+  
+  bindSocket(portc,portd) {
+    const self = this;
+    const udpServerCtrl = dgram.createSocket('udp6');
+    udpServerCtrl.on('listening', () => {
+      //console.log('DHTUdp::bindSocket: listening udpServerCtrl =<',udpServerCtrl,'>');
+    });
+    udpServerCtrl.on('message', (message, remote) =>{
+      //console.log('DHTUdp::bindSocket: message message =<',message.toString(),'>');
+      //console.log('DHTUdp::bindSocket: message remote =<',remote,'>');
+      try {
+        const jMsg = JSON.parse(message.toString());
+        const result = this.node_.verify(jMsg);
+        //console.log('DHTUdp::bindSocket: message result =<',result,'>');
+        if(result) {
+          const node = this.node_.calcID(jMsg);
+          //console.log('DHTUdp::bindSocket: message node =<',node,'>');
+          self.onCtrlMsg_(jMsg.p,remote,node);
+        }
+      } catch(err) {
+        console.log('DHTUdp::bindSocket: message err =<',err,'>');
+      }
+    });
+    udpServerCtrl.bind(portc);
+    this.portc_ = portc;
+
+    const udpServerData = dgram.createSocket('udp6');
+    udpServerData.on('listening', () => {
+      //console.log('DHTUdp::bindSocket: listening udpServerData =<',udpServerData,'>');
+    });
+    udpServerData.on('message', (message, remote) =>{
+      //console.log('DHTUdp::bindSocket: message message =<',message.toString(),'>');
+      //console.log('DHTUdp::bindSocket: message remote =<',remote,'>');
+      try {
+        const jMsg = JSON.parse(message.toString());
+        const result = this.node_.verify(jMsg);
+        //console.log('DHTUdp::bindSocket: message result =<',result,'>');
+        if(result) {
+          const node = this.node_.calcID(jMsg);
+          //console.log('DHTUdp::bindSocket: message node =<',node,'>');
+          self.onDataMsg_(jMsg.p,remote,node);
+        }
+      } catch(err) {
+        console.log('DHTUdp::bindSocket: message err =<',err,'>');
+      }
+    });
+    udpServerData.bind(portd);
+    this.portd_ = portd;
+  }
+  send(cmd,port,host) {
+    const signedCmd = this.node_.sign(cmd);
+    const cmdMsg = Buffer.from(JSON.stringify(signedCmd));
+    try {
+      this.client_.send(cmdMsg, 0, cmdMsg.length, port,host);
+    } catch(err) {
+      console.log('DHTUdp::send:err=<',err,'>');
+    }
+  }
+  broadcastSubscribe(outgates,channel,address) {
+    //console.log('DHTUdp::broadcastSubscribe: outgates =<',outgates,'>');
+    //console.log('DHTUdp::broadcastSubscribe: channel =<',channel,'>');
+    const msgDHT = {
+      subscribe:{
+        channel:channel,
+        address:address,
+        node:this.node_.id,
+        footprint:[
+          this.node_.id
+        ]
+      }
+    }
+    const outEPs = {};
+    for(const gate of outgates) {
+      //console.log('DHTUdp::broadcastSubscribe: gate =<',gate,'>');
+      if(this.node_.id !== gate) {
+        outEPs[gate] = this.worldNodes_[gate];
+      }
+    }
+    //console.log('DHTUdp::broadcastSubscribe: outEPs =<',outEPs,'>');
+    for(const outEPIndex in outEPs) {
+      const outEP = outEPs[outEPIndex];
+      //console.log('DHTUdp::broadcastSubscribe: outEP =<',outEP,'>');
+      this.send(msgDHT,outEP.portd,outEP.address);
+    }
+  }
+
+  broadcastPublish(outgates,channel,address,msg,cb) {
+    //console.log('DHTUdp::broadcastPublish: outgates =<',outgates,'>');
+    //console.log('DHTUdp::broadcastPublish: channel =<',channel,'>');
+    const msgDHT = {
+      publish:{
+        channel:channel,
+        address:address,
+        msg:msg,
+        cb,cb,
+        footprint:[
+          this.node_.id
+        ]
+      }
+    }
+    const outEPs = {};
+    for(const gate of outgates) {
+      //console.log('DHTUdp::broadcastPublish: gate =<',gate,'>');
+      if(this.node_.id !== gate) {
+        outEPs[gate] = this.worldNodes_[gate];
+      }
+    }
+    //console.log('DHTUdp::broadcastPublish: outEPs =<',outEPs,'>');
+    for(const outEPIndex in outEPs) {
+      const outEP = outEPs[outEPIndex];
+      //console.log('DHTUdp::broadcastPublish: outEP =<',outEP,'>');
+      this.send(msgDHT,outEP.portd,outEP.address);
+    }
+  }
+
+  
+  enterMesh_() {
+    //console.log('DHTUdp::enterMesh_: this.conf_ =<',this.conf_,'>');
+    for(const entrance of this.conf_.entrances) {
+      //console.log('DHTUdp::enterMesh_: entrance =<',entrance,'>');
+      const entryMesh = {
+        entry:{
+          portc:this.portc_,
+          portd:this.portd_,
+          trap:this.conf_.trap,
+          at:new Date(),
+        }
+      }
+      this.send(entryMesh,entrance.portc,entrance.host);
+    }
+  }
+  onCtrlMsg_(msg,remote,nodeFrom) {
+    //console.log('DHTUdp::onCtrlMsg_:msg=<',msg,'>');
+    //console.log('DHTUdp::onCtrlMsg_:remote=<',remote,'>');
+    //console.log('DHTUdp::onCtrlMsg_:nodeFrom=<',nodeFrom,'>');
+    if(msg.entry) {
+      this.onDHTEntry(msg.entry,remote,nodeFrom);
+    } else if(msg.welcome) {
+      this.onDHTWelcome(msg.welcome,remote,nodeFrom);
+    } else if(msg.ping) {
+      this.onDHTPing(msg.ping,remote,nodeFrom);
+    } else if(msg.pong) {
+      this.onDHTPong(msg.pong,remote,nodeFrom);
+    } else {
+      console.log('DHTUdp::onCtrlMsg_:msg=<',msg,'>');
+      console.log('DHTUdp::onCtrlMsg_:remote=<',remote,'>');
+      console.log('DHTUdp::onCtrlMsg_:nodeFrom=<',nodeFrom,'>');
+    }
+  }
+  onDHTEntry(entry,remote,nodeFrom) {
+    console.log('DHTUdp::onDHTEntry:entry=<',entry,'>');
+    //console.log('DHTUdp::onDHTEntry:remote=<',remote,'>');
+    //console.log('DHTUdp::onDHTEntry:nodeFrom=<',nodeFrom,'>');
+    const address = remote.address;
+    const portc = entry.portc;
+    const portd = entry.portd;
+    //console.log('DHTUdp::onDHTEntry:address=<',address,'>');
+    //console.log('DHTUdp::onDHTEntry:portc=<',portc,'>');
+    this.worldNodes_[nodeFrom] = {
+      address:address,
+      portc:portc,
+      portd:portd,
+      trap:entry.trap,
+      at:new Date().toISOString()
+    }
+    const welcome = {
+      welcome:{
+        nodes:this.worldNodes_,
+        at:new Date().toISOString()
+      }
+    };
+    this.send(welcome,portc,address);
+    //console.log('DHTUdp::onDHTEntry:this.worldNodes_=<',this.worldNodes_,'>');
+  }
+  onDHTWelcome(welcome,remote,nodeFrom) {
+    //console.log('DHTUdp::onDHTWelcome:welcome=<',welcome,'>');
+    //console.log('DHTUdp::onDHTWelcome:remote=<',remote,'>');
+    //console.log('DHTUdp::onDHTWelcome:nodeFrom=<',nodeFrom,'>');
+    for(const nodeKey in welcome.nodes) {
+      //console.log('DHTUdp::onDHTWelcome:nodeKey=<',nodeKey,'>');
+      const endpoint = welcome.nodes[nodeKey];
+      //console.log('DHTUdp::onDHTWelcome:endpoint=<',endpoint,'>');
+      this.worldNodes_[nodeKey] = endpoint;
+    }
+    //console.log('DHTUdp::onDHTWelcome:this.worldNodes_=<',this.worldNodes_,'>');
+  }
+  onDHTPing(ping,remote,nodeFrom) {
+    //console.log('DHTUdp::onDHTPing:ping=<',ping,'>');
+    //console.log('DHTUdp::onDHTPing:remote=<',remote,'>');
+    //console.log('DHTUdp::onDHTPing:nodeFrom=<',nodeFrom,'>');
+    const node = this.worldNodes_[nodeFrom];
+    //console.log('DHTUdp::onDHTPing:node=<',node,'>');
+    if(node) {
+      const pong = {
+        pong:{
+          s:ping.s,
+          r:new Date()
+        }
+      };
+      this.send(pong,node.portc,node.address);
+    }
+  }
+
+  onDHTPong(pong,remote,nodeFrom) {
+    //console.log('DHTUdp::onDHTPong:pong=<',pong,'>');
+    //console.log('DHTUdp::onDHTPong:remote=<',remote,'>');
+    //console.log('DHTUdp::onDHTPong:nodeFrom=<',nodeFrom,'>');
+    const endpoint = this.worldNodes_[nodeFrom];
+    //console.log('DHTUdp::onDHTPong:endpoint=<',endpoint,'>');
+    if(endpoint) {
+      const ttl = new Date() - new Date(pong.s);
+      //console.log('DHTUdp::onDHTPong:ttl=<',ttl,'>');
+      endpoint.ttl = ttl;
+      endpoint.at = pong.r;
+      //console.log('DHTUdp::onDHTPong:nodeFrom=<',nodeFrom,'>');
+      //console.log('DHTUdp::onDHTPong:endpoint=<',endpoint,'>');
+      this.bucket_.update(nodeFrom,endpoint);
+    }
+    for(const nodeCheck in this.worldNodes_) {
+      //console.log('DHTUdp::onDHTPong:nodeCheck=<',nodeCheck,'>');
+      if(nodeCheck !== this.node_.id) {
+        const endpointCheck = this.worldNodes_[nodeCheck];
+        //console.log('DHTUdp::onDHTPong:endpointCheck=<',endpointCheck,'>');
+        const escape_ms = new Date() - new Date(endpointCheck.at);
+        //console.log('DHTUdp::onDHTPong:escape_ms=<',escape_ms,'>');
+        if(escape_ms > NODE_LOST_TIME_OUT_MS) {
+          //console.log('DHTUdp::onDHTPong:escape_ms=<',escape_ms,'>');
+          //console.log('DHTUdp::onDHTPong:nodeCheck=<',nodeCheck,'>');
+          //console.log('DHTUdp::onDHTPong:endpointCheck=<',endpointCheck,'>');
+          if(endpointCheck.timeout > 0) {
+            endpointCheck.timeout++;
+          } else {
+            endpointCheck.timeout = 1;
+          }
+          if(endpointCheck.timeout > NODE_LOST_TIME_OUT_MAX) {
+            delete this.worldNodes_[nodeCheck];
+          }
+          this.bucket_.remove(nodeCheck);
+        }
+      }
+    }
+  }
+
+  
+  doDHTPing_() {
+    //console.log('DHTUdp::doDHTPing_:this.worldNodes_=<',this.worldNodes_,'>');
+    for(const nodeKey in this.worldNodes_) {
+      //console.log('DHTUdp::doDHTPing_:nodeKey=<',nodeKey,'>');
+      const node = this.worldNodes_[nodeKey];
+      //console.log('DHTUdp::doDHTPing_:node=<',node,'>');
+      if(nodeKey !== this.node_.id) {
+        //console.log('DHTUdp::doDHTPing_:node=<',node,'>');
+        //console.log('DHTUdp::doDHTPing_:nodeKey=<',nodeKey,'>');
+        const pingDHT = {
+          ping:{
+            s:new Date()
+          }
+        };
+        this.send(pingDHT,node.portc,node.address);
+      }
+    }
+  }
+
+
+
+
+
+  onDataMsg_(msg,remote,node) {
+    //console.log('DHTUdp::onDataMsg_:msg=<',msg,'>');
+    //console.log('DHTUdp::onDataMsg_:remote=<',remote,'>');
+    //console.log('DHTUdp::onDataMsg_:node=<',node,'>');
+    if(typeof this.onMsg_ === 'function') {
+      this.onMsg_(msg,remote,node);
+    }
+  }
+};
+module.exports = DHTUdp;
